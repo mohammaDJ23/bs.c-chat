d88e7bac10a5455bf4d08dc056570e1b87ce21ba
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2023-11-05 08:10:53 +0330

    initiate the app

---
+++ src/bootstrap.tsx
@@ -0,0 +1,23 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { isDevelopment } from './lib';
+
+function app(el: Element) {
+  const root = ReactDOM.createRoot(el);
+
+  return {
+    mount() {
+      root.render(<App />);
+    },
+    unMount() {
+      root.unmount();
+    },
+  };
+}
+
+if (isDevelopment()) {
+  const el = document.querySelector('#_chat-service');
+  if (el) app(el).mount();
+}
+
+export { app };
