72d378fa01df813eccb6c5d231f6880aaa50b16e
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2023-11-05 08:15:16 +0330

    adding the components

---
+++ src/components/OwnerChat/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const OwnerChat: FC = () => {
+  return <div>Owner chat</div>;
+};
+
+export default OwnerChat;
---
+++ src/components/UserChat/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const UserChat: FC = () => {
+  return <div>User chat</div>;
+};
+
+export default UserChat;
