7480ec0a5bd6c7e1aefddcd2707781e20f576591
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2023-11-20 09:51:43 +0330

    adding a filter form for searching the owners

--- src/lib/forms/index.ts
+++ src/lib/forms/index.ts
@@ -1,6 +1,8 @@
 import { UserListFilters } from './userListFilters';
+import { OwnerListFilters } from './ownerListFilters';
 
 export * from './formConstructor';
 export * from './userListFilters';
+export * from './ownerListFilters';
 
-export const forms = { UserListFilters };
+export const forms = { UserListFilters, OwnerListFilters };
