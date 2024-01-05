ea6acb06bc3aaf3bde249eedf949ec1d8f2da4e1
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2024-01-05 09:42:36 +0330

    reseting the conversation list

--- src/lib/providers/GetConversationListProvider.tsx
+++ src/lib/providers/GetConversationListProvider.tsx
@@ -115,6 +115,7 @@ const GetConversationListProvider: FC<PropsWithChildren> = ({ children }) => {
   );
 
   useEffect(() => {
+    conversationListInstance.resetList();
     getConversationList({ isInitialApi: true });
   }, []);
 
